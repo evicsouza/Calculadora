911e31b304a317ee9058e01a72801da4daae56c8
evicsouza <evavic.ifpe@gmail.com>
2022-09-20 17:21:08 -0300

    refatoração

--- app/src/main/java/com/evavictoria/calculadora/MainActivity.java
+++ app/src/main/java/com/evavictoria/calculadora/MainActivity.java
@@ -1,90 +1,151 @@
 package com.evavictoria.calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.Button;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.evavictoria.calculadora.databinding.ActivityMainBinding;
+import com.evavictoria.calculadora.calculadora.Calculadora;
+import com.evavictoria.calculadora.calculadora.Operacao;
+import com.evavictoria.calculadora.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    Double primeiroValor;
-    Double segundoValor;
-    String resultado;
-
-    ActivityMainBinding binding;
-
+    private Calculadora calculadora = new Calculadora();
+    private TextView visor;
+    private TextView visorPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
-
-
-        binding.buttonUm.setOnClickListener(v -> {
-            atualizaVisor("1", true);
-        });
-        binding.buttonDois.setOnClickListener(v -> {
-            atualizaVisor("2", true);
-                });
-        binding.buttonTres.setOnClickListener(v-> {
-                    atualizaVisor("3", true);
-                });
-        binding.buttonQuatro.setOnClickListener(v-> {
-            atualizaVisor("4", true);
-                });
-        binding.buttonCinco.setOnClickListener(v -> {
-            atualizaVisor("5", true);
-                });
-        binding.buttonSeis.setOnClickListener(v -> {
-            atualizaVisor("6", true);
-                });
-        binding.buttonSete.setOnClickListener(v -> {
-                    atualizaVisor("7", true);
-                });
-        binding.buttonOito.setOnClickListener(v -> {
-            atualizaVisor("8", true);
-                });
-        binding.buttonNove.setOnClickListener(v -> {
-            atualizaVisor("9", true);
-                });
-        binding.buttonZero.setOnClickListener(v -> {
-            atualizaVisor("0", true);
-        });
-
-
-
-
-        binding.buttonAdicao.setOnClickListener(v -> {
-            atualizaVisor("+", false);
-        });
-        binding.buttonSubtracao.setOnClickListener(v -> {
-            atualizaVisor("-", false);
-        });
-        binding.buttonMultiplicacao.setOnClickListener(v -> {
-            atualizaVisor("*", false);
-        });
-        binding.buttonDivisao.setOnClickListener(v -> {
-            atualizaVisor("/", false);
-        });
-
+        setContentView(R.layout.activity_main);
+        this.visor = (TextView) findViewById(R.id.visor);
+        this.visorPrincipal = (TextView) findViewById(R.id.visorPrincipal);
+        atualizarVisor();
     }
 
-    public void atualizaVisor(String string, Boolean limpaDados){
-        if(!resultado.isEmpty()){
-            binding.textViewResultado.setText("");
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putSerializable("calculadora", this.calculadora);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.getSerializable("calculadora") != null) {
+            this.calculadora = (Calculadora) savedInstanceState.getSerializable("calculadora");
+            atualizarVisor();
         }
-        if(limpaDados){
-            resultado = "";
-        }else{
-            binding.textViewResultado.append(resultado.toString());
-            binding.textViewResultado.append(string);
-            resultado = "";
+    }
+
+    private void setCaracter(char caracter) {
+        try {
+            calculadora.setCaracter(caracter);
+            atualizarVisor();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Ocorreu um erro!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void atualizarVisor() {
+        if (this.calculadora != null) {
+            visor.setText(calculadora.getValorTexto());
+            visorPrincipal.setText(calculadora.getValorTextoPrincipal());
+        } else {
+            visor.setText("");
+            visorPrincipal.setText("0");
+        }
+    }
+
+    private void setOperacao(Operacao operation) {
+        calculadora.setOperacao(operation);
+        atualizarVisor();
+    }
+
+    public void handleButtonUm(View view) {
+        setCaracter('1');
+    }
+
+    public void handleButtonDois(View view) {
+        setCaracter('2');
+    }
+
+    public void handleButtonTres(View view) {
+        setCaracter('3');
+    }
+
+    public void handleButtonQuatro(View view) {
+        setCaracter('4');
+    }
+
+    public void handleButtonCinco(View view) {
+        setCaracter('5');
+    }
+
+    public void handleButtonSeis(View view) {
+        setCaracter('6');
+    }
+
+    public void handleButtonSete(View view) {
+        setCaracter('7');
+    }
+
+    public void handleButtonOito(View view) {
+        setCaracter('8');
+    }
+
+    public void handleButtonNove(View view) {
+        setCaracter('9');
+    }
+
+    public void handleButtonZero(View view) {
+        setCaracter('0');
+    }
+
+    public void handleButtonSoma(View view) {
+        setOperacao(Operacao.ADICAO);
+    }
+
+    public void handleButtonSubtrai(View view) {
+        setOperacao(Operacao.SUBTRACAO);
+    }
+
+    public void handleButtonMultiplica(View view) {
+        setOperacao(Operacao.MULTIPLICACAO);
+    }
+
+    public void handleButtonDivide(View view) {
+        setOperacao(Operacao.DIVISAO);
+    }
+
+    public void handleButtonPorcentagem(View view) {
+        setOperacao(Operacao.PORCENTAGEM);
+    }
+
+    public void handleButtonVirgula(View view) {
+        setCaracter(',');
+    }
+
+    public void handleButtonResultado(View view) {
+        calculadora.calcular();
+        atualizarVisor();
+    }
+
+    public void handleButtonLimpar(View view) {
+        calculadora = new Calculadora();
+        atualizarVisor();
+    }
+
+    public void handleButtonDesfazer(View view) {
+        try {
+            calculadora.removerUltimoCaracter();
+            atualizarVisor();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "Ocorreu um erro!", Toast.LENGTH_SHORT).show();
         }
     }
 }
